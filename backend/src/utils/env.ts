6c82dc4c12306871ce0033d4164b9f3144fe61e2
Ardhika <ardhikaptr11@gmail.com>
2025-06-17 20:44:47 +0700

    feat: add email SMTP configuration to environment variables

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -2,8 +2,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const SECRET: string = process.env.SECRET || "";
-const MONGODB_CONNECTION_STRING: string = process.env.MONGODB_CONNECTION_STRING || "";
-const MONGODB_DATABASE: string = process.env.MONGODB_DATABASE || "";
-
-export { SECRET, MONGODB_CONNECTION_STRING, MONGODB_DATABASE };
+export const SECRET: string = process.env.SECRET || "";
+export const MONGODB_CONNECTION_STRING: string = process.env.MONGODB_CONNECTION_STRING || "";
+export const MONGODB_DATABASE: string = process.env.MONGODB_DATABASE || "";
+export const EMAIL_SMTP_SECURE: boolean = Boolean(process.env.EMAIL_SMTP_SECURE) || false;
+export const EMAIL_SMTP_USER: string = process.env.EMAIL_SMTP_USER || "";
+export const EMAIL_SMTP_PASS: string = process.env.EMAIL_SMTP_PASS || "";
+export const EMAIL_SMTP_PORT: number = Number(process.env.EMAIL_SMTP_PORT) || 465;
+export const EMAIL_SMTP_HOST: string = process.env.EMAIL_SMTP_HOST || "";
+export const EMAIL_SMTP_SERVICE: string = process.env.EMAIL_SMTP_SERVICE || "";
+export const CLIENT_HOST: string = process.env.CLIENT_HOST || "http://localhost:8765"
