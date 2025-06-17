53697cae35b923fa67a8b3766de4545403a3eaf5
Ardhika <ardhikaptr11@gmail.com>
2025-06-17 20:44:59 +0700

    feat: implement email utility with nodemailer and EJS template rendering

---
+++ backend/src/utils/mail/mail.ts
@@ -0,0 +1,93 @@
+import nodemailer from "nodemailer";
+import path from "path";
+import ejs from "ejs";
+
+import {
+	EMAIL_SMTP_SECURE,
+	EMAIL_SMTP_USER,
+	EMAIL_SMTP_PASS,
+	EMAIL_SMTP_PORT,
+	EMAIL_SMTP_HOST,
+	EMAIL_SMTP_SERVICE
+} from "../env";
+
+export interface ISendMail {
+	from: string;
+	to: string;
+	subject: string;
+	html: string;
+}
+
+let transporter;
+
+try {
+	const config = {
+		service: EMAIL_SMTP_SERVICE,
+		host: EMAIL_SMTP_HOST,
+		port: EMAIL_SMTP_PORT,
+		secure: EMAIL_SMTP_SECURE,
+		auth: {
+			user: EMAIL_SMTP_USER,
+			pass: EMAIL_SMTP_PASS
+		},
+		requireTLS: true
+	};
+
+	transporter = nodemailer.createTransport(config);
+} catch (error) {
+	console.error("Email transporter failed to create:", error);
+
+	interface SendEmailOptions extends ISendMail {
+		[key: string]: any;
+	}
+
+	interface MockTransporter {
+		sendMail: (options: SendEmailOptions) => Promise<{ id: string; response: string }>;
+		verify: () => Promise<boolean>;
+	}
+
+	transporter = {
+		sendMail: (options: SendEmailOptions): Promise<{ id: string; response: string }> => {
+			console.log("This email should be sent to:", {
+				from: options.from,
+				to: options.to,
+				subject: options.Subject
+			});
+			return Promise.resolve({ id: "mockId", response: "mockResponse" });
+		},
+		verify: (): Promise<boolean> => Promise.resolve(true)
+	} as MockTransporter;
+}
+
+export const verifyEmailConfig = async () => {
+	try {
+		if (!EMAIL_SMTP_HOST) throw new Error("Host is not defined");
+		if (!EMAIL_SMTP_PORT) throw new Error("Port is not defined");
+		if (!EMAIL_SMTP_USER) throw new Error("User is not defined");
+		if (!EMAIL_SMTP_PASS) throw new Error("Password is not defined");
+
+		await transporter.verify();
+		console.log("✅ Server is ready to send messages");
+	} catch (error: any) {
+		console.error("❌ Error with email configurations:", error.message);
+
+		if (error.code === "EAUTH") throw new Error("Invalid email credentials");
+		if (error.code === "ECONNECTION") throw new Error("Connection error");
+		if (error.code === "EENVELOPE") throw new Error("Invalid email envelope");
+		if (error.code === "ETIMEDOUT") throw new Error("Connection timed out");
+
+		console.warn("⚠️ Email functionality may not work as expected");
+	}
+};
+
+export const sendMail = async ({ ...emailParams }: ISendMail) => {
+	const result = await transporter.sendMail({ ...emailParams });
+
+	return result;
+};
+
+export const renderMailHtml = async (template: string, data: any): Promise<string> => {
+	const content = await ejs.renderFile(path.join(__dirname, `templates/${template}`), data);
+
+	return content as string;
+};
