c586da40b207e078e82ca866d781169d345008bc
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-19 13:00:36 +0700

    chore: update PageHead title for consistency and improve formatting

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,9 +6,11 @@ import PageHead from "@/components/commons/PageHead";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-	return (
-		<main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-			<PageHead title="Home"/>
-		</main>
-	);
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        >
+            <PageHead title="Zentix | Home"/>
+        </main>
+    );
 }
