bc0c6d3f53301c4687ad9ceb17e2d832e08c9482
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-20 00:53:29 +0700

    feat: implement DashboardLayout with sidebar for admin and member roles

---
+++ frontend/src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,50 @@
+import PageHead from "@/components/commons/PageHead";
+import { ReactNode, useState } from "react";
+import DashboardLayoutSidebar from "./DashboardLayoutSidebar";
+import { SIDEBAR_ADMIN, SIDEBAR_MEMBER } from "./Dashboard.constants";
+import { Navbar, NavbarMenuToggle } from "@heroui/react";
+
+interface PropTypes {
+    children: ReactNode;
+    title: string;
+    description?: string;
+    type?: string;
+}
+
+const DashboardLayout = (props: PropTypes) => {
+    const { children, title, description, type = "admin" } = props;
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <PageHead title={title} />
+            <div className="max-w-screen-3xl:container flex">
+                <DashboardLayoutSidebar
+                    sidebarItems={
+                        type === "admin" ? SIDEBAR_ADMIN : SIDEBAR_MEMBER
+                    }
+                    isOpen={isOpen}
+                />
+                <div className="h-screen w-full overflow-y-auto p-8">
+                    <Navbar
+                        className="flex justify-between px-0"
+                        isBlurred={false}
+                        position="static"
+                        classNames={{ wrapper: "p-0" }}
+                    >
+                        <h1 className="text-3xl font-bold">{title}</h1>
+                        <NavbarMenuToggle
+                            aria-label={isOpen ? "Close Menu" : "Open Menu"}
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="lg:hidden"
+                        />
+                    </Navbar>
+                    <p className="mb-4 text-small">{description}</p>
+                    {children}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default DashboardLayout;
