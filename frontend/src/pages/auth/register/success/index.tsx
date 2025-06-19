295e210cc7a53dd66f63507857dffe8042b28fde
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-19 13:03:59 +0700

    feat: add Register and RegisterSuccess pages for user registration flow

---
+++ frontend/src/pages/auth/register/success/index.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import RegisterSuccess from "@/components/views/Auth/RegisterSuccess/RegisterSuccess";
+
+const RegisterSuccessPage = () => {
+    return (
+        <AuthLayout title="Zentix | Registration Success">
+            <RegisterSuccess></RegisterSuccess>
+        </AuthLayout>
+    );
+};
+
+export default RegisterSuccessPage;
