29d46b1e400e3015abc83c557c038543e1365ef3
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-21 21:33:22 +0700

    feat: update env configuration to include Cloudinary credentials and fix CLIENT_HOST declaration

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -3,12 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const SECRET: string = process.env.SECRET || "";
+export const CLIENT_HOST: string = process.env.CLIENT_HOST || "http://localhost:8765"
+
 export const MONGODB_CONNECTION_STRING: string = process.env.MONGODB_CONNECTION_STRING || "";
 export const MONGODB_DATABASE: string = process.env.MONGODB_DATABASE || "";
+
 export const EMAIL_SMTP_SECURE: boolean = Boolean(process.env.EMAIL_SMTP_SECURE) || false;
 export const EMAIL_SMTP_USER: string = process.env.EMAIL_SMTP_USER || "";
 export const EMAIL_SMTP_PASS: string = process.env.EMAIL_SMTP_PASS || "";
 export const EMAIL_SMTP_PORT: number = Number(process.env.EMAIL_SMTP_PORT) || 465;
 export const EMAIL_SMTP_HOST: string = process.env.EMAIL_SMTP_HOST || "";
 export const EMAIL_SMTP_SERVICE: string = process.env.EMAIL_SMTP_SERVICE || "";
-export const CLIENT_HOST: string = process.env.CLIENT_HOST || "http://localhost:8765"
+
+export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || ""
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || ""
+export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || ""
