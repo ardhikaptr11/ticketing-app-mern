a158755607e5b1c80f038dfd796120eaa51dabd1
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-22 12:08:00 +0700

    feat: enhance Swagger documentation for media routes

--- backend/src/routes/media.router.ts
+++ backend/src/routes/media.router.ts
@@ -7,20 +7,81 @@ import { authenticate } from "../middlewares/auth.middleware";
 import { deleteFile, uploadMultipleFile, uploadSingleFile } from "../controllers/media.controller";
 import { ROLES } from "../utils/constant";
 import { checkUploadError } from "../middlewares/error.middleware";
-// import { checkUploadError } from "../middlewares/error.middleware";
 
 const router = express.Router();
 
 router.post(
 	"/media/upload-single",
-	[authenticate, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]), mediaMiddleware.single("image"), checkUploadError],
-	uploadSingleFile
+	[authenticate, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]), mediaMiddleware.single("image"), checkUploadError]
+	/**
+	  #swagger.tags = ["Media"]
+	  #swagger.security = [{
+	    "bearerAuth": []
+	  }]
+	  #swagger.requestBody = {
+	    required: true,
+		content: {
+		  "multipart/form-data": {
+		    schema: {
+			  type: "object",
+			  properties: {
+			    image: {
+				  type: "string",
+				  format: "binary"
+				}
+			  }
+			}
+		  }
+		}
+	  }
+	 */
 );
 router.post(
 	"/media/upload-batch",
 	[authenticate, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]), mediaMiddleware.multiple("images"), checkUploadError],
 	uploadMultipleFile
+	/**
+	  #swagger.tags = ["Media"]
+	  #swagger.security = [{
+	    "bearerAuth": []
+	  }]
+	  #swagger.requestBody = {
+	    required: true,
+		content: {
+		  "multipart/form-data": {
+		    schema: {
+			  type: "object",
+			  properties: {
+			    images: {
+				  type: "array",
+				  items: {
+				    type: "string",
+					format: "binary"
+				  }
+				}
+			  }
+			}
+		  }
+		}
+	  }
+	 */
 );
-router.delete("/media/delete-file", [authenticate, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])], deleteFile);
+router.delete(
+	"/media/delete-file",
+	[authenticate, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+	deleteFile
+	/**
+	  #swagger.tags = ["Media"]
+	  #swagger.security: [{
+	    "bearerAuth": []
+	  }]
+	  #swagger.requestBody: {
+	    required: true,
+		schema: {
+		  $ref: "#/components/schemas/DeleteMediaRequest"
+		}
+	  }
+	 */
+);
 
 export default router;
