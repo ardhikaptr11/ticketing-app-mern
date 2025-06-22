362679dccffabb3d028ac1b90aa2d31d57a926ef
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-22 12:07:12 +0700

    refactor: enhance Swagger documentation for category routes

--- backend/src/routes/category.router.ts
+++ backend/src/routes/category.router.ts
@@ -6,10 +6,64 @@ import { ROLES } from "../utils/constant";
 
 const router = express.Router();
 
-router.post("/category", [authenticate, aclMiddleware([ROLES.ADMIN])], create);
-router.get("/category", findAll);
-router.get("/category/:id", findOne);
-router.put("/category/:id", [authenticate, aclMiddleware([ROLES.ADMIN])], update);
-router.delete("/category/:id", [authenticate, aclMiddleware([ROLES.ADMIN])], remove);
+router.post(
+	"/category",
+	[authenticate, aclMiddleware([ROLES.ADMIN])],
+	create
+	/**
+      #swagger.tags = ["Category"]
+      #swagger.security = [{
+        "bearerAuth": []
+      }]
+      #swagger.requestBody = {
+        required: true,
+        schema: {
+          $ref: "#/components/schemas/CreateCategoryRequest"
+        }
+      }
+     */
+);
+router.get(
+    "/category", 
+    findAll
+    /**
+      #swagger.tags = ["Category"]
+     */
+);
+router.get(
+	"/category/:id",
+	findOne
+	/**
+      #swagger.tags = ["Category"]
+     */
+);
+router.put(
+	"/category/:id",
+	[authenticate, aclMiddleware([ROLES.ADMIN])],
+	update
+	/**
+      #swagger.tags = ["Category"]
+      #swagger.security = [{
+        "bearerAuth": []
+      }]
+      #swagger.requestBody = {
+        required: true,
+        schema: {
+          $ref: "#/components/schemas/CreateCategoryRequest"
+        }
+      }
+     */
+);
+router.delete(
+	"/category/:id",
+	[authenticate, aclMiddleware([ROLES.ADMIN])],
+	remove
+	/**
+      #swagger.tags = ["Category"]
+      #swagger.security = [{
+        "bearerAuth": []
+      }]
+     */
+);
 
 export default router;
