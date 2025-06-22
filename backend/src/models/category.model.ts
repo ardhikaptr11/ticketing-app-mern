eb11c2bed1d6f7fdce1e3ef0bc7bd9d53839e751
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-22 12:06:21 +0700

    fix: reposition Schema declaration in category model

--- backend/src/models/category.model.ts
+++ backend/src/models/category.model.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import * as Yup from "yup";
 
+const Schema = mongoose.Schema;
+
 // Data Access Object (DAO) for Category
 export const categoryDAO = Yup.object({
 	name: Yup.string().required("Category name is required"),
 	description: Yup.string().required("Category description is required"),
 	icon: Yup.string().required("Category icon is required")
 });
-
-const Schema = mongoose.Schema;
 
 export type Category = Yup.InferType<typeof categoryDAO>;
 
