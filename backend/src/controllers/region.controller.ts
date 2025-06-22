7ab51ef8025d320aca01e9f9c74b4c037c4fdbc4
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-22 12:04:59 +0700

    feat: add region controller with methods for city, province, regency, district, and village retrieval

---
+++ backend/src/controllers/region.controller.ts
@@ -0,0 +1,67 @@
+import { NextFunction, Request, Response } from "express";
+import response from "../utils/response";
+import { RegionModel } from "../models/region.model";
+
+export const findByCity = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { name } = req.query;
+		const result = await RegionModel.findByCity(`${name}`);
+		response.success(res, result, "Success get region by city name");
+	} catch (error: any) {
+		error.message = "Failed to get region by city name";
+		next(error);
+	}
+};
+
+export const getAllProvinces = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const result = await RegionModel.getAllProvinces();
+		response.success(res, result, "Success get all provinces");
+	} catch (error: any) {
+		error.message = "Failed to get all provinces";
+	}
+};
+
+export const getProvince = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { id } = req.params;
+		const result = await RegionModel.getProvince(Number(id));
+		response.success(res, result, "Success get a province");
+	} catch (error: any) {
+		error.message = "Failed to get province";
+		next(error);
+	}
+};
+
+export const getRegency = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { id } = req.params;
+		const result = await RegionModel.getRegency(Number(id));
+		response.success(res, result, "Success get regencies");
+	} catch (error: any) {
+		error.message = "Failed to get regency";
+		next(error);
+	}
+};
+
+export const getDistrict = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { id } = req.params;
+		const result = await RegionModel.getDistrict(Number(id));
+		response.success(res, result, "Success get districts");
+	} catch (error: any) {
+		error.message = "Failed to get district";
+		next(error);
+	}
+};
+
+export const getVillage = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { id } = req.params;
+		const result = await RegionModel.getVillage(Number(id));
+		response.success(res, result, "Success get villages");
+	} catch (error: any) {
+		error.message = "Failed to get village";
+		next(error);
+	}
+};
