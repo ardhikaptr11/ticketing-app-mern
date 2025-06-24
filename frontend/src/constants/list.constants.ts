ac447c2ea302a54d80de83f5b3e28620a137a92b
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-24 11:02:54 +0700

    feat: replace list.constants.ts with updated limit values and constants

---
+++ frontend/src/constants/list.constants.ts
@@ -0,0 +1,11 @@
+const LIMIT_LISTS = [
+    { label: "5", value: 5 },
+    { label: "10", value: 10 },
+    { label: "15", value: 15 },
+];
+const ALLOWED_LIMITS = LIMIT_LISTS.map((item) => item.label);
+const LIMIT_DEFAULT = LIMIT_LISTS[0].value;
+const PAGE_DEFAULT = 1;
+const DELAY = 500;
+
+export { LIMIT_LISTS, ALLOWED_LIMITS, LIMIT_DEFAULT, PAGE_DEFAULT, DELAY };
