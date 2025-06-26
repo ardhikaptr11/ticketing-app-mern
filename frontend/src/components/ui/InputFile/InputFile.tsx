ca510981243e2d1ef645d8e26203b65d79ecd3f6
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-26 11:50:18 +0700

    feat: enhance InputFile component with upload and delete functionality, improved error handling, and loading state

--- frontend/src/components/ui/InputFile/InputFile.tsx
+++ frontend/src/components/ui/InputFile/InputFile.tsx
@@ -1,17 +1,36 @@
-import { cn } from "@heroui/react";
+import { Button, cn, Spinner } from "@heroui/react";
 import Image from "next/image";
 import React, { ChangeEvent, useEffect, useId, useRef, useState } from "react";
 import { CiSaveUp2 } from "react-icons/ci";
+import { IoTrashOutline } from "react-icons/io5";
 
 interface PropTypes {
-    name: string;
-    isDropable?: boolean;
+    errorMessage?: string;
     className?: string;
+    isDropable?: boolean;
+    isInvalid?: boolean;
+    name: string;
+    onUpload?: (file: FileList) => void;
+    onDelete?: () => void;
+    isUploading?: boolean;
+    isDeleting?: boolean;
+    preview?: string;
 }
 
 const InputFile = (props: PropTypes) => {
-    const [uploadedImage, setUploadedImage] = useState<File | null>(null);
-    const { className, isDropable = false, name } = props;
+    const {
+        errorMessage,
+        className,
+        isDropable = false,
+        isInvalid,
+        isDeleting,
+        isUploading,
+        name,
+        onUpload,
+        onDelete,
+        preview,
+    } = props;
+    
     const drop = useRef<HTMLLabelElement>(null);
     const dropzoneId = useId();
 
@@ -24,7 +43,10 @@ const InputFile = (props: PropTypes) => {
 
     const handleDrop = (e: DragEvent) => {
         e.preventDefault();
-        setUploadedImage(e.dataTransfer?.files?.[0] || null);
+        const files = e.dataTransfer?.files;
+        if (files && onUpload) {
+            onUpload(files);
+        }
     };
 
     useEffect(() => {
@@ -41,55 +63,83 @@ const InputFile = (props: PropTypes) => {
         }
     }, []);
 
-    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleOnUpload = (e: ChangeEvent<HTMLInputElement>) => {
         const files = e.currentTarget.files;
-        if (files && files.length > 0) {
-            setUploadedImage(files[0]);
+        if (files && onUpload) {
+            onUpload(files);
         }
     };
 
     return (
-        <label
-            ref={drop}
-            htmlFor={`dropzone-file-${dropzoneId}`}
-            className={cn(
-                "mt-2 flex min-h-24 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100",
-                className,
-            )}
-        >
-            {uploadedImage ? (
-                <div className="flex flex-col items-center justify-center p-5">
-                    <div className="mb-2 w-1/2">
-                        <Image
-                            fill
-                            src={URL.createObjectURL(uploadedImage)}
-                            alt="image"
-                            className="!relative"
-                        />
+        <div>
+            <label
+                ref={drop}
+                htmlFor={`dropzone-file-${dropzoneId}`}
+                className={cn(
+                    "mt-2 flex min-h-24 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100",
+                    className,
+                    { "border-danger-500": isInvalid },
+                )}
+            >
+                {preview && (
+                    <div className="relative flex flex-col items-center justify-center p-5">
+                        <div className="mb-2 w-1/2">
+                            <Image
+                                fill
+                                src={preview}
+                                alt="image"
+                                className="!relative"
+                            />
+                        </div>
+                        <Button
+                            isIconOnly
+                            onPress={onDelete}
+                            disabled={isDeleting}
+                            className="absolute right-2 top-2 flex h-9 w-9 items-center justify-center rounded bg-danger-100"
+                        >
+                            {isDeleting ? (
+                                <Spinner size="sm" color="danger" />
+                            ) : (
+                                <IoTrashOutline className="h-5 w-5 text-danger-500" />
+                            )}
+                        </Button>
+                    </div>
+                )}
+                {!preview && !isUploading && (
+                    <div className="flex flex-col items-center justify-center p-5">
+                        <CiSaveUp2 className="mb-2 h-10 w-10 text-gray-400" />
                         <p className="tex-sm text-center font-semibold text-gray-500">
-                            {uploadedImage.name}
+                            {isDropable
+                                ? "Drag and drop or click here to upload file"
+                                : "Click to upload file here"}
                         </p>
                     </div>
-                </div>
-            ) : (
-                <div className="flex flex-col items-center justify-center p-5">
-                    <CiSaveUp2 className="mb-2 h-10 w-10 text-gray-400" />
-                    <p className="tex-sm text-center font-semibold text-gray-500">
-                        {isDropable
-                            ? "Drag and drop or click here to upload file"
-                            : "Click to upload file here"}
-                    </p>
-                </div>
+                )}
+                {isUploading && (
+                    <div className="flex flex-col items-center justify-center p-5">
+                        <Spinner color="danger" />
+                    </div>
+                )}
+                <input
+                    name={name}
+                    type="file"
+                    className="hidden"
+                    id={`dropzone-file-${dropzoneId}`}
+                    accept="image/*"
+                    onChange={handleOnUpload}
+                    disabled={preview !== ""}
+                    onClick={(e) => {
+                        e.currentTarget.value = "";
+                        e.target.dispatchEvent(
+                            new Event("change", { bubbles: true }),
+                        );
+                    }}
+                />
+            </label>
+            {isInvalid && (
+                <p className="p-1 text-xs text-danger-500">{errorMessage}</p>
             )}
-            <input
-                name={name}
-                type="file"
-                className="hidden"
-                id={`dropzone-file-${dropzoneId}`}
-                accept="image/*"
-                onChange={handleOnChange}
-            />
-        </label>
+        </div>
     );
 };
 
