c6e5ed5631a40f591821b5d2e5a774e727d7448d
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-28 23:16:26 +0700

    feat: update getEvents function to include query parameters in API request

--- frontend/src/services/event.service.ts
+++ frontend/src/services/event.service.ts
@@ -2,7 +2,7 @@ import instance from "@/libs/axios/instance";
 import endpoint from "./endpoint.constant";
 
 const eventServices = {
-    getEvents: (params?: string) => instance.get(`${endpoint.EVENT}`),
+    getEvents: (params?: string) => instance.get(`${endpoint.EVENT}?${params}`),
 };
 
 export default eventServices;
