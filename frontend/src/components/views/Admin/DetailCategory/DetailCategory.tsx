07d4d09d72cdabada82d6496c0053febae9d3231
Ardhika Putra <ardhikaptr11@gmail.com>
2025-06-28 19:34:26 +0700

    feat: add unsaved changes handling to DetailCategory component

--- frontend/src/components/views/Admin/DetailCategory/DetailCategory.tsx
+++ frontend/src/components/views/Admin/DetailCategory/DetailCategory.tsx
@@ -1,10 +1,12 @@
 import { Tab, Tabs } from "@heroui/react";
-import React from "react";
+import React, { useState } from "react";
 import IconTab from "./IconTab";
 import InfoTab from "./InfoTab";
 import useDetailCategory from "./useDetailCategory";
 
 const DetailCategory = () => {
+    const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+
     const {
         dataCategory,
         handleUpdateCategoryInfo,
@@ -12,14 +14,17 @@ const DetailCategory = () => {
         isSuccessMutateUpdateCategoryInfo,
     } = useDetailCategory();
 
+    const disabledTabs = hasUnsavedChanges ? ["info"] : [];
+
     return (
-        <Tabs aria-label="Options">
+        <Tabs aria-label="Options" disabledKeys={disabledTabs}>
             <Tab key="icon" title="Icon">
                 <IconTab
                     currentIcon={dataCategory?.icon}
                     onUpdate={handleUpdateCategoryInfo}
                     isPendingUpdate={isPendingMutateUpdateCategoryInfo}
                     isSuccessUpdate={isSuccessMutateUpdateCategoryInfo}
+                    setHasUnsavedChanges={setHasUnsavedChanges}
                 />
             </Tab>
             <Tab key="info" title="Info">
