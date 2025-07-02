160cf699a7759b65d9ee8ef9539da2ec661744a8
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-02 23:54:22 +0700

    feat: implement DetailEvent component with tabs for event management

---
+++ frontend/src/components/views/Admin/DetailEvent/DetailEvent.tsx
@@ -0,0 +1,56 @@
+import { Tab, Tabs } from "@heroui/react";
+import React, { useState } from "react";
+import CoverTab from "./BannerTab";
+import InfoTab from "./InfoTab";
+import useDetailEvent from "./useDetailEvent";
+import LocationTab from "./LocationTab";
+
+const DetailEvent = () => {
+    const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+
+    const {
+        dataDefaultRegion,
+        dataEvent,
+        handleUpdateEventInfo,
+        handleUpdateEventLocation,
+        isPendingDefaultRegion,
+        isPendingMutateUpdateEventInfo,
+        isSuccessMutateUpdateEventInfo,
+    } = useDetailEvent();
+
+    const disabledTabs = hasUnsavedChanges ? ["info", "location"] : [];
+
+    return (
+        <Tabs aria-label="Options" disabledKeys={disabledTabs}>
+            <Tab key="banner" title="Banner">
+                <CoverTab
+                    currentBanner={dataEvent?.banner}
+                    onUpdate={handleUpdateEventInfo}
+                    isPendingUpdate={isPendingMutateUpdateEventInfo}
+                    isSuccessUpdate={isSuccessMutateUpdateEventInfo}
+                    setHasUnsavedChanges={setHasUnsavedChanges}
+                />
+            </Tab>
+            <Tab key="info" title="Info">
+                <InfoTab
+                    dataEvent={dataEvent}
+                    onUpdate={handleUpdateEventInfo}
+                    isPendingUpdate={isPendingMutateUpdateEventInfo}
+                    isSuccessUpdate={isSuccessMutateUpdateEventInfo}
+                />
+            </Tab>
+            <Tab key="location" title="Location">
+                <LocationTab
+                    dataDefaultRegion={dataDefaultRegion?.data.data[0].name}
+                    dataEvent={dataEvent}
+                    onUpdate={handleUpdateEventLocation}
+                    isPendingDefaultRegion={isPendingDefaultRegion}
+                    isPendingUpdate={isPendingMutateUpdateEventInfo}
+                    isSuccessUpdate={isSuccessMutateUpdateEventInfo}
+                />
+            </Tab>
+        </Tabs>
+    );
+};
+
+export default DetailEvent;
---
+++ frontend/src/components/views/Admin/DetailEvent/useDetailEvent.tsx
@@ -0,0 +1,178 @@
+import { ToasterContext } from "@/contexts/ToasterContext";
+import eventServices from "@/services/event.service";
+import { IEvent, IEventForm } from "@/types/Event";
+import { standardizeDate } from "@/utils/date";
+import { DateValue } from "@heroui/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+
+const useDetailEvent = () => {
+    const { query, isReady } = useRouter();
+    const { setToaster } = useContext(ToasterContext);
+
+    const getEventById = async (id: string) => {
+        const { data } = await eventServices.getEventById(id);
+        return data.data;
+    };
+
+    const { data: dataEvent, refetch: refetchEvent } = useQuery({
+        queryKey: ["Event"],
+        queryFn: () => getEventById(`${query.id}`),
+        enabled: isReady,
+    });
+
+    const { data: dataDefaultRegion, isPending: isPendingDefaultRegion } =
+        useQuery({
+            queryKey: ["defaultRegion", dataEvent?.location?.region],
+            queryFn: () =>
+                eventServices.getRegencyById(dataEvent?.location?.region),
+            enabled: !!dataEvent?.location?.region,
+        });
+
+    const updateEvent = async (payload: IEvent) => {
+        const { data } = await eventServices.updateEvent(
+            `${query.id}`,
+            payload,
+        );
+        return data.data;
+    };
+
+    const {
+        mutate: mutateUpdateEventInfo,
+        isPending: isPendingMutateUpdateEventInfo,
+        isSuccess: isSuccessMutateUpdateEventInfo,
+    } = useMutation({
+        mutationFn: (payload: IEvent) => updateEvent(payload),
+        onError: (error: AxiosError<{ meta: { message: string } }>) => {
+            const message = error.response!.data.meta.message;
+
+            setToaster({
+                type: "error",
+                message,
+            });
+        },
+        onSuccess: () => {
+            refetchEvent();
+            setToaster({
+                type: "success",
+                message: "Event successfully updated",
+            });
+        },
+    });
+
+    const handleUpdateEvent = (data: IEvent) => mutateUpdateEventInfo(data);
+
+    const handleUpdateEventInfo = async (data: IEventForm) => {
+        const inputToCompare = [
+            "name",
+            "slug",
+            "category",
+            "startDate",
+            "endDate",
+            "isPublished",
+            "isFeatured",
+            "description",
+        ] as const;
+
+        const payload = {
+            ...data,
+            isFeatured: data.isFeatured === "true" ? true : false,
+            isPublished: data.isPublished === "true" ? true : false,
+            startDate: data.startDate
+                ? standardizeDate(data.startDate as DateValue)
+                : "",
+            endDate: data.endDate
+                ? standardizeDate(data.endDate as DateValue)
+                : "",
+        };
+
+        const event = await getEventById(`${query.id}`);
+
+        const dataFromDb = {
+            name: event.name,
+            slug: event.slug,
+            category: event.category,
+            startDate: event.startDate,
+            endDate: event.endDate,
+            isPublished: event.isPublished,
+            isFeatured: event.isFeatured,
+            description: event.description,
+        };
+
+        const isEqual = inputToCompare.every(
+            (item) => payload[item] === dataFromDb[item],
+        );
+
+        if (isEqual) {
+            return setToaster({
+                type: "error",
+                message: "At least one field must be changed",
+            });
+        }
+
+        mutateUpdateEventInfo(payload);
+    };
+
+    const handleUpdateEventLocation = async (data: IEventForm) => {
+        const inputToCompare = ["isOnline", "location"] as const;
+
+        const payload = {
+            isOnline: data.isOnline === "true" ? true : false,
+            location: {
+                region: `${data.region}`,
+                coordinates: [Number(data.latitude), Number(data.longitude)],
+            },
+        };
+
+        const event = await getEventById(`${query.id}`);
+
+        const dataFromDb = {
+            isOnline: event.isOnline,
+            location: {
+                region: `${event.location.region}`,
+                coordinates: [
+                    event.location.coordinates[0],
+                    event.location.coordinates[1],
+                ],
+            },
+        };
+
+        const isEqual = inputToCompare.every((item) => {
+            if (item === "location") {
+                return (
+                    payload[item].region === dataFromDb[item].region &&
+                    payload[item].coordinates[0] ===
+                        dataFromDb[item].coordinates[0] &&
+                    payload[item].coordinates[1] ===
+                        dataFromDb[item].coordinates[1]
+                );
+            } else {
+                return payload[item] === dataFromDb[item];
+            }
+        });
+
+        if (isEqual) {
+            return setToaster({
+                type: "error",
+                message: "At least one field must be changed",
+            });
+        }
+
+        mutateUpdateEventInfo(payload);
+    };
+
+    return {
+        dataDefaultRegion,
+        dataEvent,
+        handleUpdateEvent,
+        handleUpdateEventInfo,
+        handleUpdateEventLocation,
+        isPendingDefaultRegion,
+        isPendingMutateUpdateEventInfo,
+        isSuccessMutateUpdateEventInfo,
+    };
+};
+
+export default useDetailEvent;
