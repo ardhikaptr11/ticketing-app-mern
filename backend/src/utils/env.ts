8538065010fb46c7409ddd8c7cf686c4c5bf2a63
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-15 12:37:10 +0700

    fix: add missing semicolons and newlines in env.ts for consistency

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const NODE_ENV: string = process.env.NODE_ENV || "development";
 export const SECRET: string = process.env.SECRET || "";
-export const CLIENT_HOST: string = process.env.CLIENT_HOST || "http://localhost:8765"
+export const CLIENT_HOST: string = process.env.CLIENT_HOST || "http://localhost:8765";
 
 export const MONGODB_CONNECTION_STRING: string = process.env.MONGODB_CONNECTION_STRING || "";
 export const MONGODB_DATABASE: string = process.env.MONGODB_DATABASE || "";
@@ -16,6 +16,9 @@ export const EMAIL_SMTP_PORT: number = Number(process.env.EMAIL_SMTP_PORT) || 46
 export const EMAIL_SMTP_HOST: string = process.env.EMAIL_SMTP_HOST || "";
 export const EMAIL_SMTP_SERVICE: string = process.env.EMAIL_SMTP_SERVICE || "";
 
-export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || ""
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || ""
-export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || ""
+export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || "";
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || "";
+
+export const GOOGLE_API_KEY: string = process.env.GOOGLE_API_KEY || "";
+export const GOOGLE_GEOCODE_API_URL = process.env.GOOGLE_GEOCODE_API_URL || "";
