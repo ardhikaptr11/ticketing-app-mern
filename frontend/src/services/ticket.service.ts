f3cd5f72ac0f7f1cc49d4735342c28e3a71ac8c1
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-18 09:27:03 +0700

    feat: implement ticketServices for managing ticket-related API calls

---
+++ frontend/src/services/ticket.service.ts
@@ -0,0 +1,17 @@
+import instance from "@/libs/axios/instance";
+import endpoint from "./endpoint.constant";
+import { ITicket } from "@/types/Ticket";
+
+const ticketServices = {
+    getTickets: (params?: string) =>
+        instance.get(`${endpoint.TICKET}?${params}`),
+    getTicketById: (id: string) => instance.get(`${endpoint.TICKET}/${id}`),
+    addTicket: (payload: ITicket) => instance.post(endpoint.TICKET, payload),
+    updateTicket: (id: string, payload: ITicket) =>
+        instance.put(`${endpoint.TICKET}/${id}`, payload),
+    deleteTicket: (id: string) => instance.delete(`${endpoint.TICKET}/${id}`),
+    getTicketByEvent: (eventId: string) =>
+        instance.get(`${endpoint.TICKET}/${eventId}/events`),
+};
+
+export default ticketServices;
