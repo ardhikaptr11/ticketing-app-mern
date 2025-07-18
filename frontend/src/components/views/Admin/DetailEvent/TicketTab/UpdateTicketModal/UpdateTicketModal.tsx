f42433a4327ac13e9d49bad81e4a2315d2dc0de6
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-18 09:29:29 +0700

    feat: add UpdateTicketModal component for updating ticket details

---
+++ frontend/src/components/views/Admin/DetailEvent/TicketTab/UpdateTicketModal/UpdateTicketModal.tsx
@@ -0,0 +1,208 @@
+import {
+    Autocomplete,
+    AutocompleteItem,
+    Button,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    NumberInput,
+    Skeleton,
+    Spinner,
+    Textarea,
+} from "@heroui/react";
+import { useEffect } from "react";
+import { Controller } from "react-hook-form";
+import useUpdateTicketModal from "./useUpdateTicketModal";
+import { IEvent } from "@/types/Event";
+import { ITicket } from "@/types/Ticket";
+
+interface PropTypes {
+    isOpen: boolean;
+    onClose: () => void;
+    onOpenChange: () => void;
+    refetchTicket: () => void;
+    ticketId: string;
+}
+
+const UpdateTicketModal = (props: PropTypes) => {
+    const { isOpen, onClose, onOpenChange, refetchTicket, ticketId } = props;
+
+    const {
+        control,
+        dataTicket,
+        errors,
+        handleSubmitForm,
+        handleUpdateTicket,
+        handleOnClose,
+        isPendingMutateUpdateTicket,
+        isSuccessMutateUpdateTicket,
+        setValueUpdateTicketModal,
+    } = useUpdateTicketModal(ticketId);
+
+    useEffect(() => {
+        if (isSuccessMutateUpdateTicket) {
+            onClose();
+            refetchTicket();
+        }
+    }, [isSuccessMutateUpdateTicket]);
+
+    useEffect(() => {
+        if (dataTicket) {
+            setValueUpdateTicketModal("name", `${dataTicket?.name}`);
+            setValueUpdateTicketModal(
+                "description",
+                `${dataTicket?.description}`,
+            );
+            setValueUpdateTicketModal("price", dataTicket?.price);
+            setValueUpdateTicketModal("quantity", dataTicket?.quantity);
+        }
+    }, [dataTicket]);
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            placement="center"
+            scrollBehavior="inside"
+            onClose={() => handleOnClose(onClose)}
+        >
+            <form onSubmit={handleSubmitForm(handleUpdateTicket)}>
+                <ModalContent className="m-4">
+                    <ModalHeader>Update Ticket</ModalHeader>
+                    <ModalBody>
+                        <div className="flex flex-col gap-2">
+                            <p className="text-sm font-bold">Information</p>
+                            <Skeleton isLoaded={!!dataTicket?.name}>
+                                <Controller
+                                    name="name"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Input
+                                            {...field}
+                                            className="mb-2"
+                                            label="Name"
+                                            type="text"
+                                            variant="bordered"
+                                            autoComplete="off"
+                                            isInvalid={
+                                                errors.name !== undefined
+                                            }
+                                            errorMessage={errors.name?.message}
+                                            autoFocus
+                                        />
+                                    )}
+                                />
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!!dataTicket?.description}>
+                                <Controller
+                                    name="description"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Textarea
+                                            {...field}
+                                            className="mb-2"
+                                            label="Description"
+                                            variant="bordered"
+                                            isInvalid={
+                                                errors.description !== undefined
+                                            }
+                                            errorMessage={
+                                                errors.description?.message
+                                            }
+                                        />
+                                    )}
+                                />
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!!dataTicket?.price}>
+                                <Controller
+                                    name="price"
+                                    control={control}
+                                    render={({
+                                        field: { onChange, ...field },
+                                    }) => (
+                                        <NumberInput
+                                            {...field}
+                                            onValueChange={(value) =>
+                                                onChange(value)
+                                            }
+                                            className="mb-2"
+                                            label="Price"
+                                            placeholder="0"
+                                            variant="bordered"
+                                            isInvalid={
+                                                errors.price !== undefined
+                                            }
+                                            errorMessage={errors.price?.message}
+                                            startContent={
+                                                <div className="pointer-events-none flex items-center">
+                                                    <span className="text-small text-default-500">
+                                                        Rp
+                                                    </span>
+                                                </div>
+                                            }
+                                        />
+                                    )}
+                                />
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!!dataTicket?.quantity}>
+                                <Controller
+                                    name="quantity"
+                                    control={control}
+                                    render={({
+                                        field: { onChange, ...field },
+                                    }) => (
+                                        <NumberInput
+                                            {...field}
+                                            onValueChange={(value) =>
+                                                onChange(value)
+                                            }
+                                            className="mb-2"
+                                            label="Quantity"
+                                            variant="bordered"
+                                            isInvalid={
+                                                errors.quantity !== undefined
+                                            }
+                                            errorMessage={
+                                                errors.quantity?.message
+                                            }
+                                        />
+                                    )}
+                                />
+                            </Skeleton>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            variant="flat"
+                            color="danger"
+                            onPress={() => handleOnClose(onClose)}
+                            disabled={isPendingMutateUpdateTicket}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            type="submit"
+                            color="danger"
+                            disabled={isPendingMutateUpdateTicket}
+                            className="disabled:bg-default-500"
+                        >
+                            {isPendingMutateUpdateTicket ? (
+                                <Spinner color="white" size="sm" />
+                            ) : (
+                                "Update Ticket"
+                            )}
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </form>
+        </Modal>
+    );
+};
+
+export default UpdateTicketModal;
---
+++ frontend/src/components/views/Admin/DetailEvent/TicketTab/UpdateTicketModal/useUpdateTicketModal.tsx
@@ -0,0 +1,130 @@
+import { ToasterContext } from "@/contexts/ToasterContext";
+import eventServices from "@/services/event.service";
+import ticketServices from "@/services/ticket.service";
+import { ITicket } from "@/types/Ticket";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const updateTicketSchema = yup.object().shape({
+    name: yup.string().required("Ticket name is required"),
+    description: yup.string().required("Ticket description is required"),
+    price: yup
+        .number()
+        .typeError("Price cannot be empty (min: Rp. 0)")
+        .integer("Price must be a valid integer number")
+        .min(0, "Price must be a positive integer number")
+        .required("Ticket price is required"),
+    quantity: yup
+        .number()
+        .typeError("Quantity cannot be empty (min: 1)")
+        .integer("Quantity must be a valid integer number")
+        .moreThan(0, "Quantity must be bigger than 0")
+        .required("Ticket quantity is required"),
+});
+
+const useUpdateTicketModal = (ticketId: string) => {
+    const { isReady } = useRouter();
+    const { setToaster } = useContext(ToasterContext);
+
+    const {
+        control,
+        handleSubmit: handleSubmitForm,
+        formState: { errors },
+        reset,
+        setValue: setValueUpdateTicketModal,
+    } = useForm({
+        resolver: yupResolver(updateTicketSchema),
+    });
+
+    const getTicketById = async (id: string) => {
+        const { data } = await ticketServices.getTicketById(id);
+        return data.data;
+    };
+
+    const { data: dataTicket } = useQuery({
+        queryKey: ["Ticket", ticketId],
+        queryFn: () => getTicketById(ticketId),
+        enabled: isReady && !!ticketId,
+    });
+
+    const updateTicket = async (payload: ITicket) => {
+        const res = await ticketServices.updateTicket(ticketId, payload);
+
+        return res;
+    };
+
+    const handleOnClose = (onClose: () => void) => {
+        reset({
+            name: dataTicket?.name,
+            description: dataTicket?.description,
+            price: dataTicket?.price,
+            quantity: dataTicket?.quantity,
+        });
+        onClose();
+    };
+
+    const {
+        mutate: mutateUpdateTicket,
+        isPending: isPendingMutateUpdateTicket,
+        isSuccess: isSuccessMutateUpdateTicket,
+    } = useMutation({
+        mutationFn: updateTicket,
+        onError: (error: AxiosError<{ meta: { message: string } }>) => {
+            const message = error.response!.data.meta.message;
+
+            setToaster({
+                type: "error",
+                message,
+            });
+        },
+        onSuccess: () => {
+            reset();
+            setToaster({
+                type: "success",
+                message: "Ticket successfully added",
+            });
+        },
+    });
+
+    const handleUpdateTicket = async (data: ITicket) => {
+        const inputToCompare = [
+            "name",
+            "description",
+            "price",
+            "quantity",
+        ] as const;
+
+        const prevData = await getTicketById(ticketId);
+
+        const isEqual = inputToCompare.every(
+            (item) => prevData[item] === data[item],
+        );
+
+        if (isEqual)
+            return setToaster({
+                type: "error",
+                message: "At least one field must be changed",
+            });
+
+        mutateUpdateTicket(data);
+    };
+
+    return {
+        control,
+        dataTicket,
+        errors,
+        handleSubmitForm,
+        handleUpdateTicket,
+        handleOnClose,
+        isPendingMutateUpdateTicket,
+        isSuccessMutateUpdateTicket,
+        setValueUpdateTicketModal,
+    };
+};
+
+export default useUpdateTicketModal;
