bb63bc4efc51677974bad09391517661fb219f09
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-21 14:54:42 +0700

    feat: refactor App component to include ModalProvider and SessionExpiredModalListener

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,31 +6,44 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import AppShell from "@/components/commons/AppShell";
 import { ToasterProvider } from "@/contexts/ToasterContext";
+import { onErrorHandler } from "@/libs/axios/responseHandler";
+import { ModalProvider } from "@/contexts/ModalContext";
+import { SessionExpiredModalListener } from "@/components/ui/SessionExpiredModal/SessionExpiredModal";
+
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             refetchOnWindowFocus: false,
             retry: false,
+            throwOnError(error) {
+                onErrorHandler(error);
+                return false;
+            },
+        },
+        mutations: {
+            onError: onErrorHandler,
         },
     },
 });
 
-export default function App({
-    Component,
-    pageProps: { session, ...pageProps },
-}: AppProps) {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
     return (
         <SessionProvider session={session}>
             <QueryClientProvider client={queryClient}>
                 <HeroUIProvider>
-                    <ToasterProvider>
-                        <AppShell>
-                            <Component {...pageProps} />
-                        </AppShell>
-                    </ToasterProvider>
+                    <ModalProvider>
+                        <SessionExpiredModalListener />
+                        <ToasterProvider>
+                            <AppShell>
+                                <Component {...pageProps} />
+                            </AppShell>
+                        </ToasterProvider>
+                    </ModalProvider>
                 </HeroUIProvider>
             </QueryClientProvider>
         </SessionProvider>
     );
-}
+};
+
+export default App;
