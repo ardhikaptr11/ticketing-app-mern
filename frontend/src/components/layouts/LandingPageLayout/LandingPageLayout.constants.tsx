373b642aa197e5d386b48c27efffbc5e419a7a52
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-26 14:16:31 +0700

    feat: add social media links and update explore navigation in LandingPageLayout

--- frontend/src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
+++ frontend/src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
@@ -1,9 +1,48 @@
+import {
+    FaFacebook,
+    FaInstagram,
+    FaTiktok,
+    FaXTwitter,
+    FaYoutube,
+} from "react-icons/fa6";
+
 export const NAV_ITEMS = [
     { label: "Home", href: "/" },
-    { label: "Explore", href: "/event" },
+    {
+        label: "Explore",
+        href: "/events?limit=5&page=1&category=&isOnline=&isFeatured=",
+    },
 ];
 
 export const BUTTON_ITEMS = [
     { label: "Register", href: "/auth/register", variant: "bordered" },
     { label: "Login", href: "/auth/login", variant: "solid" },
+];
+
+export const SOCIAL_ITEMS = [
+    {
+        label: "Facebook",
+        href: "https://www.facebook.com/zentix",
+        icon: <FaFacebook />,
+    },
+    {
+        label: "Instagram",
+        href: "https://www.instagram.com/zentix",
+        icon: <FaInstagram />,
+    },
+    {
+        label: "Twitter",
+        href: "https://www.x.com/zentix",
+        icon: <FaXTwitter className="h-5 w-5" />,
+    },
+    {
+        label: "TikTok",
+        href: "https://www.tiktok.com/@zentix",
+        icon: <FaTiktok />,
+    },
+    {
+        label: "Youtube",
+        href: "https://www.youtube.com/@zentix",
+        icon: <FaYoutube />,
+    },
 ];
