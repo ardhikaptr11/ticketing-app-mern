fc9c1b2e52c211a671b0fa165a62be117adfe839
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-26 14:22:46 +0700

    feat: implement event search functionality in LandingPageLayoutNavbar

--- frontend/src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/useLandingPageLayoutNavbar.tsx
+++ frontend/src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/useLandingPageLayoutNavbar.tsx
@@ -1,11 +1,19 @@
+import { DELAY } from "@/constants/list.constants";
+import { useDebounce } from "@/hooks/useDebounce";
 import authServices from "@/services/auth.service";
+import eventServices from "@/services/event.service";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const useLandingPageLayoutNavbar = () => {
     const { isReady } = useRouter();
-    const session = useSession();
+    const { status } = useSession();
+
+    const [searchEvent, setSearchEvent] = useState("");
+
+    const debounce = useDebounce();
 
     const getProfile = async () => {
         const { data } = await authServices.getProfile();
@@ -16,9 +24,39 @@ const useLandingPageLayoutNavbar = () => {
     const { data: dataProfile } = useQuery({
         queryKey: ["Profile"],
         queryFn: getProfile,
-        enabled: isReady && session.status === "authenticated",
+        enabled: isReady && status === "authenticated",
     });
 
-    return { dataProfile };
+    const handleSearchEvent = (event: string) => {
+        debounce(() => setSearchEvent(event), DELAY);
+    };
+
+    const handleClearSearch = () => setSearchEvent("");
+
+    const getEvents = async () => {
+        const { data } = await eventServices.getEvents(`search=${searchEvent}`);
+
+        return data.data;
+    };
+
+    const {
+        data: dataSearchEvents,
+        isRefetching: isRefetchingSearchEvents,
+        isLoading: isLoadingSearchEvents,
+    } = useQuery({
+        queryKey: ["searchEvent", searchEvent],
+        queryFn: getEvents,
+        enabled: searchEvent !== "",
+    });
+
+    return {
+        dataProfile,
+        dataSearchEvents,
+        handleClearSearch,
+        handleSearchEvent,
+        isRefetchingSearchEvents,
+        isLoadingSearchEvents,
+        searchEvent,
+    };
 };
 export default useLandingPageLayoutNavbar;
