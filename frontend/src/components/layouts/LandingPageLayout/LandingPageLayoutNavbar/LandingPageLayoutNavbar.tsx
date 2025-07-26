8c4e40d98473360a52c7e4be63787e85a4c278ac
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-26 14:22:12 +0700

    feat: enhance LandingPageLayoutNavbar with event search functionality and user profile dropdown

--- frontend/src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/LandingPageLayoutNavbar.tsx
+++ frontend/src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/LandingPageLayoutNavbar.tsx
@@ -5,8 +5,11 @@ import {
     Dropdown,
     DropdownItem,
     DropdownMenu,
+    DropdownSection,
     DropdownTrigger,
     Input,
+    Listbox,
+    ListboxItem,
     Navbar,
     NavbarBrand,
     NavbarContent,
@@ -14,31 +17,44 @@ import {
     NavbarMenu,
     NavbarMenuItem,
     NavbarMenuToggle,
+    Skeleton,
+    User,
 } from "@heroui/react";
 import Image from "next/image";
 import { BUTTON_ITEMS, NAV_ITEMS } from "../LandingPageLayout.constants";
 import SafeLink from "@/components/commons/SafeLink";
 import { useRouter } from "next/router";
 import cn from "@/utils/cn";
-import { CiGrid41, CiLogout, CiSearch, CiUser } from "react-icons/ci";
+import { CiGrid41, CiLogout, CiSearch, CiWallet } from "react-icons/ci";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
 import useLandingPageLayoutNavbar from "./useLandingPageLayoutNavbar";
-import { ImInsertTemplate } from "react-icons/im";
 import { Fragment } from "react";
+import { IEvent } from "@/types/Event";
+import { displayEventDateTime } from "@/utils/date";
+import { FaArrowRight } from "react-icons/fa6";
 
 const LandingPageLayoutNavbar = () => {
     const router = useRouter();
-    const session = useSession();
-    const { dataProfile } = useLandingPageLayoutNavbar();
+    const { status } = useSession();
+
+    const {
+        dataProfile,
+        dataSearchEvents,
+        handleClearSearch,
+        handleSearchEvent,
+        isRefetchingSearchEvents,
+        isLoadingSearchEvents,
+        searchEvent,
+    } = useLandingPageLayoutNavbar();
 
     return (
         <Navbar
             maxWidth="full"
-            className="max-w-screen-3xl 3xl:container"
             isBordered
             isBlurred={false}
-            shouldHideOnScroll
+            className="p-4"
+            classNames={{ wrapper: "p-0" }}
         >
             <div className="flex items-center gap-8">
                 <NavbarBrand as={SafeLink} href="/">
@@ -61,7 +77,11 @@ const LandingPageLayoutNavbar = () => {
                                 "font-medium text-default-700 hover:text-danger",
                                 {
                                     "font-bold text-danger-500":
-                                        router.pathname === item.href,
+                                        item.label === "Explore"
+                                            ? router.asPath.startsWith(
+                                                  "/events",
+                                              )
+                                            : router.pathname === "/",
                                 },
                             )}
                         >
@@ -72,29 +92,137 @@ const LandingPageLayoutNavbar = () => {
             </div>
             <NavbarContent justify="end">
                 <NavbarMenuToggle className="lg:hidden" />
-                <NavbarItem className="hidden lg:relative lg:flex">
-                    <Input
-                        className="w-[300px]"
-                        placeholder="Search event"
-                        startContent={<CiSearch />}
-                        onClear={() => {}}
-                        onChange={() => {}}
-                        isClearable
-                    />
-                </NavbarItem>
+                <div className="relative">
+                    <NavbarItem className="hidden lg:relative lg:flex">
+                        <Input
+                            className="w-[300px]"
+                            placeholder="Search event"
+                            startContent={<CiSearch />}
+                            onClear={handleClearSearch}
+                            onChange={(e) => handleSearchEvent(e.target.value)}
+                            isClearable
+                        />
+                    </NavbarItem>
+                    {searchEvent !== "" && (
+                        <Listbox
+                            aria-label="Event found box"
+                            className="fixed top-20 max-w-[300px] rounded-lg bg-white"
+                            itemClasses={{
+                                base: "gap-4 data-[hover=true]:bg-default-100/80",
+                            }}
+                            emptyContent="Sorry, no one has created that event."
+                        >
+                            {isLoadingSearchEvents ||
+                            isRefetchingSearchEvents ? (
+                                <ListboxItem
+                                    key="skeleton-search-event"
+                                    startContent={
+                                        <Skeleton className="h-16 w-20 rounded-md" />
+                                    }
+                                >
+                                    <div className="flex flex-col justify-center gap-2">
+                                        <Skeleton className="h-3 w-4/5 rounded-2xl" />
+                                        <Skeleton className="h-3 w-3/5 rounded-2xl" />
+                                    </div>
+                                </ListboxItem>
+                            ) : (
+                                dataSearchEvents?.map((data: IEvent) => (
+                                    <ListboxItem
+                                        key={data._id}
+                                        startContent={
+                                            <Image
+                                                src={`${data.banner}`}
+                                                alt={`${data.name}`}
+                                                width={80}
+                                                height={40}
+                                                className="rounded-md"
+                                            />
+                                        }
+                                        endContent={<FaArrowRight />}
+                                        href={`/event/${data.slug}`}
+                                    >
+                                        <div className="flex flex-col justify-center">
+                                            <h3 className="line-clamp-1">
+                                                {data.name}
+                                            </h3>
+                                            <p className="text-default-400">
+                                                {
+                                                    displayEventDateTime(
+                                                        `${data.startDate}`,
+                                                        `${data.endDate}`,
+                                                    ).split(",")[0]
+                                                }
+                                            </p>
+                                        </div>
+                                    </ListboxItem>
+                                ))
+                            )}
+                        </Listbox>
+                    )}
+                </div>
 
-                {session.status === "authenticated" ? (
-                    <NavbarItem className="hidden lg:block">
+                {status === "authenticated" ? (
+                    <NavbarItem className="hidden lg:flex">
                         <Dropdown placement="bottom-start">
                             <DropdownTrigger>
                                 <Avatar
                                     isBordered
                                     showFallback
-                                    className="cursor-pointer"
+                                    as="button"
+                                    className="transition-transform"
                                     src={dataProfile?.profilePicture}
                                 />
                             </DropdownTrigger>
-                            <DropdownMenu>
+                            <DropdownMenu
+                                aria-label="Profile Actions"
+                                variant="flat"
+                            >
+                                <DropdownSection
+                                    showDivider
+                                    aria-label="Profile"
+                                >
+                                    <DropdownItem
+                                        key="profile"
+                                        isReadOnly={
+                                            dataProfile?.role === "admin"
+                                                ? true
+                                                : false
+                                        }
+                                        className="h-14 gap-2 opacity-100"
+                                        href={
+                                            dataProfile?.role === "user"
+                                                ? "/member/profile"
+                                                : undefined
+                                        }
+                                    >
+                                        <User
+                                            avatarProps={{
+                                                size: "sm",
+                                                src: dataProfile?.profilePicture,
+                                                showFallback: true,
+                                            }}
+                                            classNames={{
+                                                name: "text-default-600",
+                                                description: "text-default-500",
+                                            }}
+                                            name={dataProfile?.fullName}
+                                            description={
+                                                <p>
+                                                    @{dataProfile?.username}{" "}
+                                                    <span
+                                                        className={cn({
+                                                            hidden:
+                                                                dataProfile?.role !==
+                                                                "admin",
+                                                        })}
+                                                    >
+                                                        ({dataProfile?.role})
+                                                    </span>
+                                                </p>
+                                            }
+                                        />
+                                    </DropdownItem>
+                                </DropdownSection>
                                 <DropdownItem
                                     key="dashboard"
                                     href="/admin/dashboard"
@@ -102,22 +230,22 @@ const LandingPageLayoutNavbar = () => {
                                         hidden: dataProfile?.role !== "admin",
                                     })}
                                     startContent={
-                                        <CiGrid41 className="h-4 w-4" />
+                                        <CiGrid41 className="text-xl" />
                                     }
                                 >
                                     Dashboard
                                 </DropdownItem>
                                 <DropdownItem
-                                    key="profile"
-                                    href="/member/profile"
+                                    key="transactions"
+                                    href="/member/transactions"
                                     className={cn({
                                         hidden: dataProfile?.role !== "user",
                                     })}
                                     startContent={
-                                        <CiUser className="h-4 w-4" />
+                                        <CiWallet className="text-xl" />
                                     }
                                 >
-                                    Profile
+                                    Transactions
                                 </DropdownItem>
                                 <DropdownItem
                                     key="logout"
@@ -137,7 +265,7 @@ const LandingPageLayoutNavbar = () => {
                         {BUTTON_ITEMS.map((item) => (
                             <NavbarItem key={`button-${item.label}`}>
                                 <Button
-                                    as={Link}
+                                    as={SafeLink}
                                     href={item.href}
                                     variant={
                                         item.variant as ButtonProps["variant"]
@@ -166,7 +294,7 @@ const LandingPageLayoutNavbar = () => {
                             <Link href={item.href}>{item.label}</Link>
                         </NavbarMenuItem>
                     ))}
-                    {session.status === "authenticated" ? (
+                    {status === "authenticated" ? (
                         <Fragment>
                             <NavbarMenuItem
                                 className={cn(
