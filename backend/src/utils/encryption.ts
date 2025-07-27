d5c37524ab2676f9b2aacbf3dcf9e2eb67db2857
Ardhika Putra <ardhikaptr11@gmail.com>
2025-07-27 13:22:30 +0700

    fix: correct formatting and add new method in encryption utility

--- backend/src/utils/encryption.ts
+++ backend/src/utils/encryption.ts
@@ -1,8 +1,14 @@
-import crypto from 'crypto';
-import { SECRET } from './env';
+import crypto from "crypto";
+import { SECRET } from "./env";
 
 export const encrypt = (password: string): string => {
-    const encrypted = crypto.pbkdf2Sync(password, SECRET, 1000, 64, "sha512").toString("hex")
+	const encrypted = crypto.pbkdf2Sync(password, SECRET, 1000, 64, "sha512").toString("hex");
 
-    return encrypted
-}
+	return encrypted;
+};
+
+export const compare = (password: string, currentHashedPassword: string) => {
+	const hashToCompare = crypto.pbkdf2Sync(password, SECRET, 1000, 64, "sha512").toString("hex");
+
+	return hashToCompare === currentHashedPassword;
+};
