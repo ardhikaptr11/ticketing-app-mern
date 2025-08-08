b175104c4d994cf4f9898be932a06428a09357b9
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-08 19:39:58 +0700

    fix: add getEventBySlug method to event services

--- frontend/src/services/event.service.ts
+++ frontend/src/services/event.service.ts
@@ -5,6 +5,8 @@ import { IEvent } from "@/types/Event";
 const eventServices = {
     getEvents: (params?: string) => instance.get(`${endpoint.EVENT}?${params}`),
     getEventById: (id: string) => instance.get(`${endpoint.EVENT}/${id}`),
+    getEventBySlug: (slug: string) =>
+        instance.get(`${endpoint.EVENT}/${slug}/slug`),
     addEvent: (payload: IEvent) => instance.post(endpoint.EVENT, payload),
     updateEvent: (id: string, payload: IEvent) =>
         instance.put(`${endpoint.EVENT}/${id}`, payload),
