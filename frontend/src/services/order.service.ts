a501a6191166d8ef3d620771d9f2cdea1034a803
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-08 20:01:29 +0700

    feat: implement order services for managing orders and payment status

---
+++ frontend/src/services/order.service.ts
@@ -0,0 +1,15 @@
+import instance from "@/libs/axios/instance";
+import endpoint from "./endpoint.constant";
+import { ICart } from "@/types/Ticket";
+
+const orderServices = {
+    createOrder: (payload: ICart[]) =>
+        instance.post(`${endpoint.ORDER}`, payload),
+    updatePaymentStatus: (id: string, status: string) =>
+        instance.put(`${endpoint.ORDER}/${id}/${status}`),
+    getOrdersHistory: (params: string) => instance.get(`${endpoint.ORDER}-history?${params}`),
+    getOrderById: (id: string) => instance.get(`${endpoint.ORDER}/${id}`),
+    deleteOrder: (id: string) => instance.delete(`${endpoint.ORDER}/${id}`)
+};
+
+export default orderServices;
