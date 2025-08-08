f8f895c039282320ac6416cbd1f838e529e4d655
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-08 19:53:18 +0700

    feat: implement Transactions component with data fetching and action handling

---
+++ frontend/src/components/views/Member/Transactions/Transactions.tsx
@@ -0,0 +1,183 @@
+import DataTable from "@/components/ui/DataTable";
+import { Chip, useDisclosure } from "@heroui/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Key, ReactNode, useCallback, useEffect } from "react";
+import { COLUMN_LIST_TRANSACTIONS } from "./Transactions.constants";
+import useTransactions from "./useTransactions";
+import DeleteTransactionModal from "./DeleteTransactionModal";
+import DropdownAction from "@/components/commons/DropdownAction";
+import {
+    ALLOWED_LIMITS,
+    LIMIT_DEFAULT,
+    PAGE_DEFAULT,
+} from "@/constants/list.constants";
+import { convertToIDR } from "@/utils/currency";
+import { IoTrashOutline, IoWallet } from "react-icons/io5";
+import { RiInformationLine } from "react-icons/ri";
+
+import { TbBasketCancel } from "react-icons/tb";
+import useChangeURL from "@/hooks/useChangeURL";
+import {
+    FaRegCircleCheck,
+    FaRegCircleXmark,
+    FaRegClock,
+} from "react-icons/fa6";
+
+const Transactions = () => {
+    const { isReady, push, query, replace } = useRouter();
+
+    const { currentLimit, currentSearch, setUrl } = useChangeURL();
+
+    const {
+        dataOrdersHistory,
+        getColorStatus,
+        isLoadingOrdersHistory,
+        isRefetchingOrdersHistory,
+        refetchTransactions,
+        selectedId,
+        setSelectedId,
+    } = useTransactions();
+
+    useEffect(() => {
+        if (isReady) {
+            if (!ALLOWED_LIMITS.includes(currentLimit as string)) {
+                replace({
+                    query: {
+                        limit: LIMIT_DEFAULT,
+                        page: PAGE_DEFAULT,
+                        search: currentSearch || "",
+                    },
+                });
+                return;
+            }
+
+            setUrl();
+        }
+    }, [isReady]);
+
+    const deleteTransactionModal = useDisclosure();
+
+    const getListOfActions = (data: Record<string, unknown>) => {
+        switch (data.status) {
+            case "completed":
+                return [
+                    {
+                        name: "detail",
+                        startContent: (
+                            <RiInformationLine className="text-medium" />
+                        ),
+                        action: () =>
+                            push(`/member/transactions/${data.orderId}`),
+                    },
+                ];
+            case "pending":
+                return [
+                    {
+                        name: "pay",
+                        startContent: <IoWallet className="text-medium" />,
+                        action: () =>
+                            push(
+                                `${(data.payment as Record<string, string>).redirect_url}`,
+                            ),
+                    },
+                    {
+                        name: "cancel",
+                        startContent: (
+                            <TbBasketCancel className="text-medium" />
+                        ),
+                        action: () =>
+                            push(
+                                `/payment?order_id=${data.orderId}&status_code=200&transaction_status=cancelled`,
+                            ),
+                    },
+                ];
+            case "cancelled":
+                return [
+                    {
+                        name: "delete",
+                        startContent: (
+                            <IoTrashOutline className="text-medium" />
+                        ),
+                        action: () => {
+                            setSelectedId(`${data.orderId}`);
+                            deleteTransactionModal.onOpen();
+                        },
+                    },
+                ];
+        }
+    };
+
+    const decideStartContent = (status: string) => {
+        switch (status) {
+            case "completed":
+                return <FaRegCircleCheck />;
+            case "pending":
+                return <FaRegClock />;
+            case "cancelled":
+                return <FaRegCircleXmark />;
+            default:
+                break;
+        }
+    };
+
+    const renderCell = useCallback(
+        (orderHistory: Record<string, unknown>, columnKey: Key) => {
+            const cellValue =
+                orderHistory[columnKey as keyof typeof orderHistory];
+
+            const listOfActions = getListOfActions(orderHistory);
+
+            switch (columnKey) {
+                case "total":
+                    return convertToIDR(cellValue as number);
+                case "status":
+                    return (
+                        <Chip
+                            color={getColorStatus(cellValue as string)}
+                            size="sm"
+                            variant="flat"
+                            className="flex items-center gap-1 capitalize leading-none"
+                            startContent={decideStartContent(
+                                cellValue as string,
+                            )}
+                        >
+                            {cellValue as string}
+                        </Chip>
+                    );
+                case "actions":
+                    return (
+                        <DropdownAction listOfActions={listOfActions ?? []} />
+                    );
+                default:
+                    return cellValue as ReactNode;
+            }
+        },
+        [push],
+    );
+
+    return (
+        <section>
+            {Object.keys(query).length > 0 && (
+                <DataTable
+                    columns={COLUMN_LIST_TRANSACTIONS}
+                    data={dataOrdersHistory?.data || []}
+                    emptyContent="Orders is empty"
+                    isLoading={
+                        isLoadingOrdersHistory || isRefetchingOrdersHistory
+                    }
+                    renderCell={renderCell}
+                    totalPages={dataOrdersHistory?.pagination.totalPages}
+                />
+            )}
+            <DeleteTransactionModal
+                refetchTransactions={refetchTransactions}
+                selectedId={selectedId}
+                setSelectedId={setSelectedId}
+                {...deleteTransactionModal}
+            />
+        </section>
+    );
+};
+
+export default Transactions;
---
+++ frontend/src/components/views/Member/Transactions/useTransactions.ts
@@ -0,0 +1,66 @@
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { Chip } from "@heroui/react";
+import { IoInformationCircleOutline } from "react-icons/io5";
+
+import useChangeURL from "@/hooks/useChangeURL";
+import orderServices from "@/services/order.service";
+
+const useTransactions = () => {
+    const [selectedId, setSelectedId] = useState("");
+    const { isReady } = useRouter();
+
+    const { currentLimit, currentPage, currentSearch, setUrl } = useChangeURL();
+
+    const getColorStatus = (status: string) => {
+        switch (status) {
+            case "completed":
+                return "success";
+            case "pending":
+                return "warning";
+            case "cancelled":
+                return "danger";
+            default:
+                break;
+        }
+    };
+
+    const getOrdersHistory = async () => {
+        const params = currentSearch
+            ? `page=${currentPage}&limit=${currentLimit}&search=${currentSearch}`
+            : `page=${currentPage}&limit=${currentLimit}`;
+
+        const res = await orderServices.getOrdersHistory(params);
+        const { data } = res;
+
+        return data;
+    };
+
+    const {
+        data: dataOrdersHistory,
+        isLoading: isLoadingOrdersHistory,
+        isRefetching: isRefetchingOrdersHistory,
+        refetch: refetchTransactions,
+    } = useQuery({
+        queryKey: ["OrdersHistory", currentPage, currentLimit, currentSearch],
+        queryFn: getOrdersHistory,
+        enabled: isReady && !!currentPage && !!currentLimit,
+    });
+
+    return {
+        currentPage,
+        currentLimit,
+        currentSearch,
+        dataOrdersHistory,
+        getColorStatus,
+        isLoadingOrdersHistory,
+        isRefetchingOrdersHistory,
+        refetchTransactions,
+        selectedId,
+        setSelectedId,
+        setUrl,
+    };
+};
+
+export default useTransactions;
