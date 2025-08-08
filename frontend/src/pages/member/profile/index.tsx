19a21d26b8e64817cb1b925d18841fcb8ace3932
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-08 19:38:15 +0700

    fix: replace Dashboard component with Profile component in MemberProfilePage

--- frontend/src/pages/member/profile/index.tsx
+++ frontend/src/pages/member/profile/index.tsx
@@ -1,10 +1,10 @@
 import DashboardLayout from "@/components/layouts/DashboardLayout";
-import Dashboard from "@/components/views/Member/Dashboard";
+import Profile from "@/components/views/Member/Profile";
 
 const MemberProfilePage = () => {
     return (
         <DashboardLayout title="Profile" description="Manage your profile and security" type="member">
-            <Dashboard />
+            <Profile />
         </DashboardLayout>
     );
 };
