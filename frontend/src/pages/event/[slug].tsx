f3354248685174724c60950c667cdd05a8acff9a
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-08 19:57:54 +0700

    feat: create DetailEventPage component with layout and event details

---
+++ frontend/src/pages/event/[slug].tsx
@@ -0,0 +1,13 @@
+import DetailEventLayout from "@/components/layouts/DetailEventLayout";
+import DetailEvent from "@/components/views/DetailEvent";
+import React from "react";
+
+const DetailEventPage = () => {
+    return (
+        <DetailEventLayout>
+            <DetailEvent />
+        </DetailEventLayout>
+    );
+};
+
+export default DetailEventPage;
