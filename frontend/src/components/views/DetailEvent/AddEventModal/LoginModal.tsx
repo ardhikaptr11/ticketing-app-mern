4ee4ef11d32cc937275fb8db41228e59387f2c83
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-08 19:44:57 +0700

    feat: add LoginModal component with authentication logic

---
+++ frontend/src/components/views/DetailEvent/AddEventModal/LoginModal.tsx
@@ -0,0 +1,154 @@
+import {
+    Button,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    Spinner,
+    useDraggable,
+} from "@heroui/react";
+import React, { Fragment, RefObject, useEffect, useRef } from "react";
+import useLoginModal from "./useLoginModal";
+import { Controller } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import Link from "next/link";
+import Image from "next/image";
+
+interface PropTypes {
+    isOpen: boolean;
+    onClose: () => void;
+    onOpenChange: () => void;
+}
+
+const LoginModal = (props: PropTypes) => {
+    const { isOpen, onClose, onOpenChange } = props;
+
+    const {
+        isVisible,
+        handleVisiblePassword,
+        control,
+        handleSubmit,
+        handleLogin,
+        handleOnClose,
+        isPendingLogin,
+        isSuccessMutateLogin,
+        errors,
+    } = useLoginModal();
+
+    useEffect(() => {
+        if (isSuccessMutateLogin) onClose();
+    }, [isSuccessMutateLogin]);
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            placement="center"
+            scrollBehavior="inside"
+            onClose={() => handleOnClose(onClose)}
+        >
+            <form
+                className="flex w-80 flex-col gap-4"
+                onSubmit={handleSubmit(handleLogin)}
+            >
+                <ModalContent>
+                    <ModalHeader>
+                        <div className="flex w-full flex-col items-center gap-2">
+                            <Image
+                                src="/images/illustrations/login.svg"
+                                alt="Login"
+                                width={200}
+                                height={200}
+                                priority
+                            />
+                            <div className="text-center">
+                                <h2 className="text-2xl font-bold text-danger-500">
+                                    Sign In to Continue
+                                </h2>
+                                <p className="mt-2 text-sm">
+                                    Don&apos;t have an account?&nbsp;
+                                    <Link
+                                        href="/auth/register"
+                                        className="text-sm font-semibold text-danger-400 hover:underline"
+                                    >
+                                        Register here
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Controller
+                            name="identifier"
+                            control={control}
+                            render={({ field }) => (
+                                <Input
+                                    {...field}
+                                    label="Email / Username"
+                                    type="text"
+                                    variant="bordered"
+                                    autoComplete="off"
+                                    isInvalid={errors.identifier !== undefined}
+                                    errorMessage={errors.identifier?.message}
+                                />
+                            )}
+                        />
+
+                        <Controller
+                            name="password"
+                            control={control}
+                            render={({ field }) => (
+                                <Input
+                                    {...field}
+                                    label="Password"
+                                    type={isVisible ? "text" : "password"}
+                                    variant="bordered"
+                                    autoComplete="off"
+                                    isInvalid={errors.password !== undefined}
+                                    errorMessage={errors.password?.message}
+                                    endContent={
+                                        <button
+                                            aria-label="toggle password visibility"
+                                            className="self-center focus:outline-none"
+                                            type="button"
+                                            onClick={handleVisiblePassword}
+                                        >
+                                            {isVisible ? (
+                                                <FaEye className="pointer-events-none text-xl text-default-400" />
+                                            ) : (
+                                                <FaEyeSlash className="pointer-events-none text-xl text-default-400" />
+                                            )}
+                                        </button>
+                                    }
+                                />
+                            )}
+                        />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            type="submit"
+                            color="danger"
+                            size="lg"
+                            disabled={isPendingLogin}
+                            fullWidth
+                        >
+                            {isPendingLogin ? (
+                                <Spinner
+                                    color="white"
+                                    size="sm"
+                                    variant="dots"
+                                />
+                            ) : (
+                                "Login"
+                            )}
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </form>
+        </Modal>
+    );
+};
+
+export default LoginModal;
---
+++ frontend/src/components/views/DetailEvent/AddEventModal/useLoginModal.tsx
@@ -0,0 +1,116 @@
+import { useContext, useState } from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+import { ILogin } from "@/types/Auth";
+import { ToasterContext } from "@/contexts/ToasterContext";
+
+const loginSchema = yup.object().shape({
+    identifier: yup
+        .string()
+        .required("Please enter your email or username")
+        .test(
+            "is-email-or-username",
+            "Invalid username or email format",
+            function (value) {
+                if (!value) return false;
+
+                const looksLikeEmail = /^[\w].*@/.test(value);
+                const isEmailValid = yup.string().email().isValidSync(value);
+                const isUsernameValid =
+                    !looksLikeEmail && value.trim().length > 0;
+
+                return isEmailValid || isUsernameValid;
+            },
+        ),
+    password: yup
+        .string()
+        .min(8, "Password at least contain 8 characters")
+        .required("Please enter your password"),
+});
+
+const useLoginModal = () => {
+    const { push, query } = useRouter();
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleVisiblePassword = () => setIsVisible(!isVisible);
+    const { setToaster } = useContext(ToasterContext);
+
+    const callbackUrl: string = `/event/${query.slug}`;
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm({
+        resolver: yupResolver(loginSchema),
+        defaultValues: {
+            identifier: "",
+            password: "",
+        },
+    });
+
+    const loginService = async (payload: ILogin) => {
+        const result = await signIn("credentials", {
+            ...payload,
+            redirect: false,
+            callbackUrl,
+        });
+
+        if (result?.error && result?.status === 401) {
+            throw new Error("Invalid username or password");
+        }
+
+        return result;
+    };
+
+    const {
+        mutate: mutateLogin,
+        isPending: isPendingLogin,
+        isSuccess: isSuccessMutateLogin,
+    } = useMutation({
+        mutationFn: loginService,
+        onError: () => {
+            setToaster({
+                type: "error",
+                message: "Invalid login credentials",
+            });
+        },
+        onSuccess: () => {
+            reset();
+            setToaster({
+                type: "success",
+                message: "Login success!",
+                afterLoginSuccess: true,
+            });
+            push(callbackUrl);
+        },
+    });
+
+    const handleLogin = (data: ILogin) => mutateLogin(data);
+
+    const handleOnClose = (onClose: () => void) => {
+        reset();
+        onClose();
+    };
+
+    return {
+        isVisible,
+        handleVisiblePassword,
+        control,
+        handleSubmit,
+        handleLogin,
+        handleOnClose,
+        isPendingLogin,
+        isSuccessMutateLogin,
+        errors,
+    };
+};
+
+export default useLoginModal;
