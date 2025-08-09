3bae47b5f2dc889dfa720b5f769383ff2137f310
Ardhika Putra <ardhikaptr11@gmail.com>
2025-08-09 11:10:42 +0700

    refactor: simplify component by removing unused hooks and improving voucher rendering

--- frontend/src/components/views/Member/DetailTransaction/DetailTransaction.tsx
+++ frontend/src/components/views/Member/DetailTransaction/DetailTransaction.tsx
@@ -1,4 +1,3 @@
-import React, { Fragment, useEffect, useState } from "react";
 import useDetailTransaction from "./useDetailTransaction";
 import {
     Button,
@@ -12,50 +11,23 @@ import { convertToIDR } from "@/utils/currency";
 import { FaRegCircleCheck } from "react-icons/fa6";
 import Image from "next/image";
 import { displayEventDateTime } from "@/utils/date";
-import { useRouter } from "next/router";
-import Ticket from "@/components/ui/Ticket";
-import { ICart } from "@/types/Ticket";
+import SafeLink from "@/components/commons/SafeLink";
+
+type TVoucher = {
+    voucherId: string;
+    isPrint: boolean;
+    _id: string;
+};
 
 const DetailTransaction = () => {
     const {
-        dataTransaction,
+        dataCategories,
         dataEvent,
         dataTickets,
-        generateQR,
-        getAllCategories,
+        dataTransaction,
         isLoadingDataTickets,
-        setQrCode,
         qrCode,
     } = useDetailTransaction();
-
-    const {
-        push,
-        query: { ticket },
-    } = useRouter();
-
-    useEffect(() => {
-        const fetchQR = async () => {
-            const dataUrl = await generateQR(
-                "https://zentix-kappa.vercel.app/",
-            );
-            if (dataUrl) setQrCode(dataUrl);
-        };
-
-        fetchQR();
-    }, []);
-
-    const [dataCategories, setDataCategories] = useState<string[]>([]);
-
-    useEffect(() => {
-        const fetchCategories = async () => {
-            if (!dataTransaction?.tickets?.length) return;
-
-            const categories = await getAllCategories(dataTransaction.tickets);
-            setDataCategories(categories);
-        };
-
-        fetchCategories();
-    }, [dataTransaction?.tickets]);
 
     return (
         <Card className="px-5 py-4">
@@ -92,7 +64,7 @@ const DetailTransaction = () => {
                                       )
                                     : dataTickets?.map((ticket, index) => (
                                           <p className="text-sm" key={index}>
-                                              {`${ticket?.name} (${convertToIDR(Number(ticket?.price))}) x ${dataTransaction?.tickets?.[index]?.quantity}`}
+                                              {`${ticket?.name} (${convertToIDR(Number(ticket?.price))}) x ${Number(dataTransaction?.tickets?.[index]?.quantity)}`}
                                           </p>
                                       ))}
                             </div>
@@ -135,79 +107,76 @@ const DetailTransaction = () => {
                 <div className="flex w-full flex-col gap-2">
                     <h4 className="font-bold">Ticket:</h4>
                     <div className="flex flex-col gap-4">
-                        {Array.from({
-                            length: dataTransaction?.vouchers.length,
-                        }).map((_, idx) => (
-                            <Card
-                                className="flex flex-col gap-2 p-5 lg:flex-row"
-                                key={`ticket-${idx}`}
-                            >
-                                {qrCode ? (
-                                    <Image
-                                        src={`${qrCode}`}
-                                        width={300}
-                                        height={300}
-                                        alt="QR Code"
-                                        className="mx-auto lg:mx-0"
-                                    />
-                                ) : (
-                                    <Skeleton className="mx-auto h-[300px] w-[300px] rounded-md lg:mx-0" />
-                                )}
-                                <div className="flex flex-col gap-2 p-1">
-                                    <h1 className="text-xl font-semibold text-danger-500">
-                                        {dataEvent?.name}
-                                    </h1>
-                                    <div className="text-sm font-semibold">
-                                        <p className="text-foreground-500">
-                                            Category
-                                        </p>
-                                        <p className="text-danger-500">
-                                            {dataCategories[idx]}
-                                        </p>
+                        {dataTransaction?.vouchers.map(
+                            (voucher: TVoucher, idx: number) => (
+                                <Card
+                                    className="flex flex-col gap-2 p-5 lg:flex-row"
+                                    key={`ticket-${idx}`}
+                                >
+                                    {qrCode ? (
+                                        <Image
+                                            src={`${qrCode}`}
+                                            width={300}
+                                            height={300}
+                                            alt="QR Code"
+                                            className="mx-auto lg:mx-0"
+                                        />
+                                    ) : (
+                                        <Skeleton className="mx-auto h-[300px] w-[300px] rounded-md lg:mx-0" />
+                                    )}
+                                    <div className="flex flex-col gap-2 p-1">
+                                        <h1 className="text-xl font-semibold text-danger-500">
+                                            {dataEvent?.name}
+                                        </h1>
+                                        <div className="text-sm font-semibold">
+                                            <p className="text-foreground-500">
+                                                Category
+                                            </p>
+                                            <p className="text-danger-500">
+                                                {dataCategories[idx]}
+                                            </p>
+                                        </div>
+                                        <div className="text-sm font-semibold">
+                                            <p className="text-foreground-500">
+                                                Date
+                                            </p>
+                                            <p className="text-danger-500">
+                                                {displayEventDateTime(
+                                                    `${dataEvent?.startDate}`,
+                                                    `${dataEvent?.endDate}`,
+                                                )}
+                                            </p>
+                                        </div>
+                                        <div className="text-sm font-semibold">
+                                            <p className="text-foreground-500">
+                                                Location
+                                            </p>
+                                            <p className="text-danger-500">
+                                                {dataEvent?.location?.address}
+                                            </p>
+                                        </div>
+                                        <Button
+                                            variant="bordered"
+                                            size="md"
+                                            color="danger"
+                                            className="max-w-[100px]"
+                                            as={SafeLink}
+                                            href={
+                                                dataEvent?.isOnline
+                                                    ? `${dataEvent?.address}`
+                                                    : `/member/transactions/${dataTransaction?.orderId}/ticket?voucher=${voucher.voucherId}`
+                                            }
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            {dataEvent?.isOnline
+                                                ? "Join Now"
+                                                : "Show Ticket"}
+                                        </Button>
                                     </div>
-                                    <div className="text-sm font-semibold">
-                                        <p className="text-foreground-500">
-                                            Date
-                                        </p>
-                                        <p className="text-danger-500">
-                                            {displayEventDateTime(
-                                                `${dataEvent?.startDate}`,
-                                                `${dataEvent?.endDate}`,
-                                            )}
-                                        </p>
-                                    </div>
-                                    <div className="text-sm font-semibold">
-                                        <p className="text-foreground-500">
-                                            Location
-                                        </p>
-                                        <p className="text-danger-500">
-                                            {dataEvent?.location?.address}
-                                        </p>
-                                    </div>
-                                    <Button
-                                        variant="bordered"
-                                        size="md"
-                                        color="danger"
-                                        className="max-w-[100px]"
-                                        onPress={
-                                            dataEvent?.isOnline
-                                                ? () =>
-                                                      push(
-                                                          `${dataEvent?.address}`,
-                                                      )
-                                                : () =>
-                                                      alert(
-                                                          "Sorry, ticket cannot be displayed at this time. We're working on it",
-                                                      )
-                                        }
-                                    >
-                                        {dataEvent?.isOnline
-                                            ? "Join Now"
-                                            : "Show Ticket"}
-                                    </Button>
-                                </div>
-                            </Card>
-                        ))}
+                                </Card>
+                            ),
+                        )}
                     </div>
                 </div>
             </CardBody>
